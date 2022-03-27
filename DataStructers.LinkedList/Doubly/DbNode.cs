34d1d093f5e8290c32846725bbb7b63f522918b7
Hasan Hüseyin Alav <hhsynalv@gmail.com>
2022-03-27 15:04:01 +0300

    DoublyLinkedList

---
+++ DataStructers.LinkedList/Doubly/DbNode.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructers.LinkedList.Doubly
+{
+    public class DbNode<T>
+    {
+        public T Value { get; set; }
+        public DbNode<T> Prev { get; set; }
+        public DbNode<T> Next { get; set; }
+
+        public DbNode(T item)
+        {
+            Value = item;
+        }
+
+        public override string ToString() => $"{Value}";
+    }
+}
